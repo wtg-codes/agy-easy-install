e66c3d4cdce86479d595316800e04dd2e204c98c
wtg-codes <wtgranstaff@gmail.com>
2026-05-07 18:00:14 -0400

    Delete setup.sh

--- setup.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-echo "📥 Fetching Antigravity Manager..."
-curl -sSL https://raw.githubusercontent.com/google/antigravity-install/main/antigravity-manager.sh -o antigravity-manager.sh
-chmod +x antigravity-manager.sh
-
-echo "🚀 Launching installer..."
-./antigravity-manager.sh --install
